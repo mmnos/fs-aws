304cf88b3c46fcc09f8184ce8502a5ee64760ffd
mmnos <mnos9897@gmail.com>
2020-08-11 18:28:51 -0400

    added some comments for api routes. changed mongo method to deleteOne for deleting a record

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Book = require("../models/Book");
 
+// GET route for all books in the database
 router.get("/books", async (req, res) => {
   try {
     const book = await Book.find();
@@ -10,19 +11,21 @@ router.get("/books", async (req, res) => {
   }
 });
 
+// POST route for users who choose to save a book
 router.post("/books", async (req, res) => {
   try {
     const book = await Book.create(req.body);
-    res.json(book);
+    res.json("This book was successfully saved to the database!");
   } catch (err) {
     res.json(err);
   }
 });
 
+// DELETE route for users who no longer want a book in their saved list
 router.delete("/books/:id", async (req, res) => {
   try {
-    const book = await Book.remove(req.params.id);
-    res.json(`This book was successfully deleted -----> ${book}`);
+    const book = await Book.deleteOne({ _id: req.params.id });
+    res.json(`This book was successfully deleted from the database!`);
   } catch (err) {
     res.json(err);
   }
