7c73c745c5ae3d397c90fa998bb2689a34cd81b1
mmnos <mnos9897@gmail.com>
2020-08-11 16:41:10 -0400

    setup get, post, delete route for books

--- server/routes/api-routes.js
+++ server/routes/api-routes.js
@@ -1,3 +1,31 @@
-const express = require("express").Router();
+const router = require("express").Router();
+const Book = require("../models/Book");
+
+router.get("/books", async (req, res) => {
+  try {
+    const book = await Book.find();
+    res.json(book);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.post("/books", async (req, res) => {
+  try {
+    const book = await Book.create(req.body);
+    res.json(book);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+router.delete("/books/:id", async (req, res) => {
+  try {
+    const book = await Book.remove(req.params.id);
+    res.json(`This book was successfully deleted -----> ${book}`);
+  } catch (err) {
+    res.json(err);
+  }
+});
 
 module.exports = router;
