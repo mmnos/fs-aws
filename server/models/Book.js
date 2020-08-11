1f0a179f8bc7bf4a901ccdc6277d96723afd7cc0
mmnos <mnos9897@gmail.com>
2020-08-11 15:41:38 -0400

    updated server script. added schema for the Book model

---
+++ server/models/Book.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: "Please provide a book title.",
+  },
+  authors: {
+    type: String,
+    required: "Please provide the name(s) of author(s).",
+  },
+  description: {
+    type: String,
+    required: "Please provide a description of the book.",
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  day: {
+    type: Date,
+    default: () => new Date(),
+  },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
