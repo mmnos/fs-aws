fb304a322cdb289dd5e751f276eadb8bafd1d082
mmnos <mnos9897@gmail.com>
2020-08-11 01:57:00 -0400

    got initial file structure for backend

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// const htmlRoutes = require("./routes/html-routes");
+// const apiRoutes = require("./routes/api-routes");
+
+// app.use("/", htmlRoutes);
+// app.use("/api", apiRoutes);
+
+const MONGO_URI = process.env.ATLAS_URI;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
