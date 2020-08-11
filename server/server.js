0b3968ea8335345f69db23a136794a89b3135f3c
mmnos <mnos9897@gmail.com>
2020-08-11 19:25:48 -0400

    added build config in server file. deleted some starter files/code in client folder

--- server/server.js
+++ server/server.js
@@ -1,26 +1,30 @@
+const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
-
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_ATLAS_URI;
+const apiRoutes = require("./routes/api-routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// const htmlRoutes = require("./routes/html-routes");
-const apiRoutes = require("./routes/api-routes");
-
-// app.use("/", htmlRoutes);
-app.use("/api", apiRoutes);
-
-const MONGO_URI = process.env.MONGO_ATLAS_URI;
 
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
+});
+
+app.use("/api", apiRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("../client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
