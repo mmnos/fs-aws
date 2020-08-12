520594bf853de1d01fcd24a918b8a8c92dd5a674
mmnos <mnos9897@gmail.com>
2020-08-12 14:27:10 -0400

    added undraw image to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react";
+import logo from "../undraw_the_world_is_mine_nb0e.svg";
 import Search from "../components/Search/Search";
+import { Row, Col } from "react-bootstrap";
 
 const Home = () => {
+  const imageStyle = {
+    height: 300,
+    width: "100%",
+    marginTop: 15,
+  };
+
   return (
     <>
-      <Search />
+      <Row className="my-auto">
+        <Col sm={{ span: 6, offset: 3 }}>
+          <img src={logo} style={imageStyle} />
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={12}>
+          <Search />
+        </Col>
+      </Row>
     </>
   );
 };
