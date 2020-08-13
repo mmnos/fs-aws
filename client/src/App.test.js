1683add0427efc9b399896e96db7f7a5a4afb828
mmnos <mnos9897@gmail.com>
2020-08-13 15:47:46 -0400

    tested running App.test

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import Saved from "./pages/Saved";
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test("renders learn react link", () => {
+  const { getByText } = render(<Saved />);
+  const linkElement = getByText(/Saved/i);
   expect(linkElement).toBeInTheDocument();
 });
