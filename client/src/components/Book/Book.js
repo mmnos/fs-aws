6697ac893923e401d4100bb8d6773c4a44f1bf3d
mmnos <mnos9897@gmail.com>
2020-08-15 22:55:14 -0400

    styled booklist on search

--- client/src/components/Book/Book.js
+++ client/src/components/Book/Book.js
@@ -1,23 +1,52 @@
 import React from "react";
+import "./Book.css";
 import { Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDollarSign, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 const Book = ({ title, authors, description, image, link }) => {
   const imageStyle = {
-    height: 200,
+    height: 400,
     width: "100%",
+    borderRadius: "1%",
   };
 
   return (
-    <Col sm={4}>
-      <img style={imageStyle} src={image} alt="thumbnail" />
-      <h2>{title}</h2>
-      <p>
-        {authors.map((author) => (
-          <span key={author}>{author},</span>
-        ))}
-      </p>
-      <p> {description} </p>
-      {link && <a href={link}>Buy Here</a>}
+    <Col sm={6}>
+      <div className="bookInfo">
+        <img style={imageStyle} src={image} alt="thumbnail" />
+        <hr />
+        <h2 className="title">{title}</h2>
+        <p className="author">
+          {" "}
+          Written by :{" "}
+          {authors.map((author) => (
+            <span key={author}>{author}, </span>
+          ))}
+        </p>
+        <hr />
+        <p className="description"> {description} </p>
+        <hr />
+        <p className="links">
+          <span>
+            {link && (
+              <a
+                className="buyLink"
+                href={link}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faDollarSign} />{" "}
+              </a>
+            )}
+          </span>
+          <span>
+            <a className="save" href="#">
+              <FontAwesomeIcon icon={faHeart} />
+            </a>
+          </span>
+        </p>
+      </div>
     </Col>
   );
 };
