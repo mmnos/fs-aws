2bfc04bcd78c9b655ae344e26b51f11651910a33
mmnos <mnos9897@gmail.com>
2020-08-17 16:10:27 -0400

    idk pt. 2

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_ATLAS_URI;
-const apiRoutes = require("./routes/api-routes");
+const apiRoutes = require("./server/routes/api-routes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,11 +20,11 @@ mongoose.connect(MONGO_URI, {
 app.use("/api", apiRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../client/build"));
+  app.use(express.static("./client/build"));
 }
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join("../client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
